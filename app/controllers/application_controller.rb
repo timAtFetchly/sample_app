c92449ac09dce50a5322fa8870d52712c244bd25
Tim <tim@fetch.ly>
2018-10-25 10:11:17 -0600

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  root 'application#hello'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: 'hello world!'
+  end
+
+
 end
